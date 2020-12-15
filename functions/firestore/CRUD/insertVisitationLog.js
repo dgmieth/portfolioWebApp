7551ffe0c6d41e368f316784bf9e73eef0bfbb24
dgmieth <dgmieth@gmail.com>
2020-12-14 23:36:46 -0300

    1- css changes to home.css and project.css; 2- home.hbs loading progress bar, cookie creation; 3- project.hbs accordion inside card; 4- functions/ directory rearranging; 5- index.js insertVisitaionLog service route created

--- functions/firestore/CRUD/insertVisitationLog.js
+++ functions/firestore/CRUD/insertVisitationLog.js
@@ -5,52 +5,50 @@ const admin = require('firebase-admin');
 //----------------------------------------------------------------------------------
 //       checks if user is already logged in, if not register login in database
 //----------------------------------------------------------------------------------
-module.exports = async (req,res,next)=>{
-    if(!req.logged){
-        req.logged = true
-        req.userString = JSON.stringify(req.headers["user-agent"])
-        await insertVisitationLog(req)
-    }
-    next()
+module.exports = async(chosenConfiguration)=>{
+    await insertVisitationLog(chosenConfiguration)
+    return null
+    
 }
 //----------------------------------------------------------------------------------
 //                      insert into database statement
 //----------------------------------------------------------------------------------
-async function insertVisitationLog(request){
+async function insertVisitationLog(chosenConfiguration){
     var newTime = new Date()
-    var docTitle = 'W_'
-    docTitle = docTitle + newTime.getUTCFullYear()
-    if((newTime.getUTCMonth() + 1) <= 9){
-        docTitle = docTitle  + '0' + (newTime.getUTCMonth() + 1)
-    } else {
-        docTitle = docTitle + (newTime.getUTCMonth()+1)
-    }
-    if(newTime.getUTCDate() <= 9){
-        docTitle = docTitle + '0' + newTime.getUTCDate()
-    } else {
-        docTitle = docTitle + newTime.getUTCDate()
-    }
-    if(newTime.getUTCHours() <= 9){
-        docTitle = docTitle + '_0' + newTime.getUTCHours()
-    } else {
-        docTitle = docTitle + '_' + newTime.getUTCHours()
-    }
-    if(newTime.getUTCMinutes() <= 9){
-        docTitle = docTitle + '0' + newTime.getUTCMinutes()
-    } else {
-        docTitle = docTitle + newTime.getUTCMinutes()
-    }
-    if(newTime.getUTCSeconds()  <= 9){
-        docTitle = docTitle + '0' + newTime.getUTCSeconds()
-    } else {
-        docTitle = docTitle + newTime.getUTCSeconds()
-    }
+    if (chosenConfiguration==='production'){
+        var docTitle = 'prod_'
+        newTime.setHours(newTime.getHours()-3)
+        docTitle = `${docTitle}${newTime.getUTCFullYear()}` 
     
-    // const writeResult = await admin.firestore().collection('logIn_log').doc(`${docTitle}`).set({
-    //     logged: request.logged,
-    //     userAgentString: request.userString,
-    //     time: new Date()
-    // })
-    // .catch((error)=>{console.error("Error writing document: ", error)})
-    
+        if((newTime.getUTCMonth() + 1) <= 9){
+            docTitle = `${docTitle}-0${(newTime.getUTCMonth() + 1)}`
+        } else {
+            docTitle = `${docTitle}-${(newTime.getUTCMonth()+1)}`
+        }
+        if(newTime.getUTCDate() <= 9){
+            docTitle = `${docTitle}-0${newTime.getUTCDate()}`
+        } else {
+            docTitle = `${docTitle}-${newTime.getUTCDate()}`
+        }
+        if(newTime.getUTCHours() <= 9){
+            docTitle = `${docTitle} 0${newTime.getUTCHours()}`
+        } else {
+            docTitle = `${docTitle} ${newTime.getUTCHours()}`
+        }
+        if(newTime.getUTCMinutes() <= 9){
+            docTitle = `${docTitle}:0${newTime.getUTCMinutes()}`
+        } else {
+            docTitle = `${docTitle}:${newTime.getUTCMinutes()}`
+        }
+        if(newTime.getUTCSeconds()  <= 9){
+            docTitle = `${docTitle}:0${newTime.getUTCSeconds()}`
+        } else {
+            docTitle = `${docTitle}:${newTime.getUTCSeconds()}`
+        }    
+        const writeResult = await admin.firestore().collection('logIn_log').doc(`${docTitle}`).set({
+            logged: true,
+            time: new Date()
+        })
+        .catch((error)=>{console.error("Error writing document: ", error)})
     }
+}
--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ const hbs = require('./_functions/hbsConfiguration')
 //database firestore configuration
 var firestoreConfiguration = require('./firestore/keyAndConfiguration/firestoreConfiguration')
 var configurationType = require('./firestore/keyAndConfiguration/configurationType')
+var chosenConfiguration = configurationType.dev
 //database statements
 var getAboutMeInformation = require('./firestore/CRUD/getAboutMeInformation')
 var getSpecificProject = require('./firestore/CRUD/getSpecificProject')
@@ -23,7 +24,6 @@ app.use(express.static(publicPath))
 app.use(express.urlencoded())
 app.use(express.json())
 //configure middleware in app
-app.use(insertVisitationLog)
 
 //==================================================================================
 //                                   ROUTING
@@ -46,26 +46,12 @@ app.get('/home/project', async (req,res)=>{
 //----------------------------------------------------------------------------------
 //                                service routes
 //----------------------------------------------------------------------------------
-//teste
- app.get('/teste',async (request,response) =>{
-     var db_result = await getFirestore();
-     response.render('teste',{db_result});
- });
-//==================================================================================
-//                                ASYNC FUNCTIONS
-//==================================================================================
-//get data from fireStore for homePage
- //async function getFirestore(){
-   //  const firestore_con  = await admin.firestore();
-     //let sfRef = firestore_con.collection('projects').doc('mobileApp').collection('apps').get().then(docs => {
-       //  docs.forEach(doc =>{
-         //    console.log('Found subcollection with id:', doc.data()['id']);
-         //})
-   // });
-    //return sfRef
-//}
+//registers log in firebase
+app.get('/registerLog', async(req, res)=>{
+    await insertVisitationLog(chosenConfiguration)
+})
 //==================================================================================
 //                            APP/SERVER INITIALIZATION
 //==================================================================================
-firestoreConfiguration(configurationType.dev)  
+firestoreConfiguration(chosenConfiguration)  
 exports.app = functions.https.onRequest(app)
